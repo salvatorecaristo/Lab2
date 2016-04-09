eef7cd8faf836a60aa9362a06a24fc163cd65a53
Salvatore Caristo <s196241@studenti.polito.it>
2016-04-09 17:54:36 +0200

    1.3

--- Lab2_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
+++ Lab2_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
@@ -1,5 +1,64 @@
 package it.polito.tdp.spellchecker.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
 public class SpellCheckerController {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<String> cmbLanguage;
+
+    @FXML
+    private TextArea txtInput;
+
+    @FXML
+    private Button btnSpellCheck;
+
+    @FXML
+    private TextArea txtOutput;
+
+    @FXML
+    private Label lblState;
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Label lblTime;
+
+    @FXML
+    void doClearText(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doSpellcheck(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert cmbLanguage != null : "fx:id=\"cmbLanguage\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert txtInput != null : "fx:id=\"txtInput\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert btnSpellCheck != null : "fx:id=\"btnSpellCheck\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert txtOutput != null : "fx:id=\"txtOutput\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert lblState != null : "fx:id=\"lblState\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert btnClear != null : "fx:id=\"btnClear\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert lblTime != null : "fx:id=\"lblTime\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        
+        cmbLanguage.getItems().addAll("Italiano","English");
+
+    }
 }
